f6f44324971101585f7ce2eb11d951c65419df9d
garima <srivgarima61@gmail.com>
2019-02-02 06:13:52 +0530

    This is saturday second commit

--- src/First/hello.java
+++ src/First/hello.java
@@ -5,6 +5,8 @@ public class hello {
 	public static void main(String[]args) {
 		
 	System.out.println("Hello java");
+	System.out.println("Hi Buddy");
+	System.out.println("How are you");
 	}
 }
 //this is to learn git **********
